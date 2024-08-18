f1a07a2e5658b95875ff5196962741d02f5cd3af
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-08-18 22:48:56 +0000

    chore: update user auth middleware

--- middlewares/userAuthMiddleware.go
+++ middlewares/userAuthMiddleware.go
@@ -11,37 +11,37 @@ import (
 )
 
 func UserAuthMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
+	return func(c *gin.Context) {
 
-        authHeader := c.GetHeader("Authorization")
-        if authHeader == "" {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing Authorization header"})
-            c.Abort()
-            return
-        }
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing Authorization header"})
+			c.Abort()
+			return
+		}
 
-        tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-        if tokenString == authHeader {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Malformed Authorization header"})
-            c.Abort()
-            return
-        }
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Malformed Authorization header"})
+			c.Abort()
+			return
+		}
 
-        claims, err := utils.ParseUserJWT(tokenString)
-        if err != nil {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-            c.Abort()
-            return
-        }
+		claims, err := utils.ParseUserJWT(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
 
-        var user models.User
-        if err := initializers.DB.Where("email = ? AND client_id = ?", claims.Email, claims.ClientId).First(&user).Error; err != nil {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found or does not belong to the client"})
-            c.Abort()
-            return
-        }
+		var user models.ClientAppUser
+		if err := initializers.DB.Where("email = ? AND client_id = ?", claims.Email, claims.ClientAppId).First(&user).Error; err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found or does not belong to the client"})
+			c.Abort()
+			return
+		}
 
-        c.Set("user", user)
-        c.Next()
-    }
+		c.Set("user", user)
+		c.Next()
+	}
 }
