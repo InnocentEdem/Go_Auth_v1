74e0a4964cd47f6c0dbca6979888838ff79e7d86
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-08-18 22:47:41 +0000

    chore: add user confirmation model

--- models/clientConfirmationMethod.model.go
+++ models/clientConfirmationMethod.model.go
@@ -7,13 +7,14 @@ import (
 	"gorm.io/gorm"
 )
 
-type ClientConfirmationMethod struct {
-	ID                   uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-	ClientID             uuid.UUID      `gorm:"type:uuid;unique"`
-	ConfirmEmail         bool           `gorm:"default:false"`
-	ConfirmPhone         bool           `gorm:"default:false"`
-	ConfirmPaymentMethod bool           `gorm:"default:false"`
-	CreatedAt            time.Time      `gorm:"autoCreateTime"`
-	UpdatedAt            time.Time      `gorm:"autoUpdateTime"`
-	DeletedAt            gorm.DeletedAt `gorm:"index"`
+type AppConfirmationMethod struct {
+	ID                   uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	ClientAppID          uuid.UUID      `gorm:"type:uuid;unique" json:"client_app_id"`
+	ConfirmEmail         bool           `gorm:"default:false" json:"confirm_email"`
+	ConfirmPhone         bool           `gorm:"default:false" json:"confirm_phone"`
+	ConfirmPaymentMethod bool           `gorm:"default:false" json:"confirm_payment_method"`
+	CreatedAt            time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt            time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt            gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
+
