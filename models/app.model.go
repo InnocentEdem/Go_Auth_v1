990d547cbe56c73158230e2927309e61d042e406
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-08-18 22:42:13 +0000

    chore: add app model

--- models/app.model.go
+++ models/app.model.go
@@ -1,19 +1,41 @@
 package models
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type App struct {
-    ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-    AppName     string    `gorm:"type:varchar(100);not null"`
-    AppPublicId string    `gorm:"type:varchar(100);not null;uniqueIndex"`
-    ClientID    uuid.UUID `gorm:"type:uuid;not null"`
-    Client      Client    `gorm:"foreignKey:ClientID;constraint:OnDelete:CASCADE;"`
-    CreatedAt   time.Time
-    UpdatedAt   time.Time
-    DeletedAt   gorm.DeletedAt `gorm:"index"`
+type ClientApp struct {
+	ID                     uuid.UUID             `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	AppName                string                `gorm:"type:varchar(100);not null" json:"app_name"`
+	ClientID               uuid.UUID             `gorm:"type:uuid;not null" json:"client_id"`
+	CreatedAt              time.Time             `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt              time.Time             `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt              gorm.DeletedAt        `gorm:"index" json:"deleted_at,omitempty"`
+	APN                    string                `gorm:"type:varchar(100);uniqueIndex" json:"apn"`
+	AppAdvancedConfig      AppAdvancedConfig     `gorm:"foreignKey:ClientAppID;constraint:OnDelete:CASCADE" json:"app_advanced_config"`
+	AppConfirmationMethods AppConfirmationMethod `gorm:"foreignKey:ClientAppID;constraint:OnDelete:CASCADE" json:"app_confirmation_methods"`
+}
+
+func (app *ClientApp) BeforeCreate(tx *gorm.DB) (err error) {
+	app.ID = uuid.New()
+
+	if app.APN, err = GenerateAPN(16); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GenerateAPN(length int) (string, error) {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("failed to generate random bytes: %w", err)
+	}
+	return hex.EncodeToString(bytes), nil
 }
